caeeda9e776c228c2eef9308fbfd9d35e33c0898
Jose Antonio Jaramago Lozano <jajl82@hotmail.com>
2022-11-06 13:28:09 +0000

    feat: added guard for authentication

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/guards';
 
 const routes: Routes = [
   {
@@ -13,7 +14,8 @@ const routes: Routes = [
   },
   {
     path: 'task-list',
-    loadChildren: () => import('./pages/task-list/task-list.module').then( m => m.TaskListPageModule)
+    loadChildren: () => import('./pages/task-list/task-list.module').then( m => m.TaskListPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'login',
